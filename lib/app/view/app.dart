616b4d4873be6cd20e6f656537a13a0a0bbd05d2
Ayush783 <ayushc45xxx@gmail.com>
2022-04-09 22:33:36 +0530

    home view integrated

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:gathrr/home/home.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const MaterialApp(
+      home: HomeView(),
+    );
   }
 }

1a1fd3a27014a57096b4e21bfe4d30a342abd431
Ayush783 <ayushc45xxx@gmail.com>
2022-04-09 22:34:01 +0530

    bloc integrated and added getevent

--- lib/home/bloc/home_bloc.dart
+++ lib/home/bloc/home_bloc.dart
@@ -1,11 +1,21 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:gathrr/home/service/home_service.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitial()) {
-    on<HomeEvent>((event, emit) {});
+    on<GetEvents>(_onGetEvents);
+  }
+
+  final HomeService _hs = HomeService();
+
+  void _onGetEvents(GetEvents event, Emitter emit) async {
+    log('getting events');
+    final response = _hs.getEvents();
   }
 }
--- lib/home/view/home.dart
+++ lib/home/view/home.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/home_bloc.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider(
+      create: (context) => HomeBloc()
+        ..add(
+          GetEvents(),
+        ),
+      child: const HomeViewBody(),
+    );
+  }
+}
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.read<HomeBloc>().add(GetEvents());
+        },
+      ),
+      body: Stack(
+        children: [],
+      ),
+    );
   }
 }

1a222459c710893668cc3b8262b3f05b567b9d95
Ayush783 <ayushc45xxx@gmail.com>
2022-04-10 22:28:20 +0530

    carousel created and integrated

--- lib/home/view/home.dart
+++ lib/home/view/home.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gathrr/home/widget/carousel.dart';
+import 'package:gathrr/theme/theme.dart';
 
 import '../bloc/home_bloc.dart';
 
@@ -9,28 +13,197 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => HomeBloc()
-        ..add(
-          GetEvents(),
-        ),
+      create: (context) => HomeBloc(),
       child: const HomeViewBody(),
     );
   }
 }
 
-class HomeViewBody extends StatelessWidget {
+class HomeViewBody extends StatefulWidget {
   const HomeViewBody({Key? key}) : super(key: key);
 
   @override
+  State<HomeViewBody> createState() => _HomeViewBodyState();
+}
+
+class _HomeViewBodyState extends State<HomeViewBody> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<HomeBloc>().add(GetEvents());
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final devicePadding = MediaQuery.of(context).padding;
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.read<HomeBloc>().add(GetEvents());
-        },
-      ),
       body: Stack(
-        children: [],
+        children: [
+          Container(
+            height: size.height / 2,
+            width: size.width,
+            color: const Color(0xff02b140),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(
+              devicePadding.left,
+              devicePadding.top,
+              devicePadding.right,
+              devicePadding.bottom,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const HomeAppbar(),
+                const SizedBox(height: 24),
+                LiveEventCard(),
+                SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    'Upcoming Events',
+                    style: kTextstyle2.copyWith(color: Colors.white),
+                  ),
+                ),
+                SizedBox(height: 16),
+                Carousel(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    CarouselIndicators(),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class LiveEventCard extends StatelessWidget {
+  const LiveEventCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Event title',
+                      style: kTextstyle2,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'another text',
+                      style: kTextstyle3.copyWith(),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'another text',
+                      style: kTextstyle3,
+                    ),
+                    const SizedBox(height: 4),
+                    TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                          backgroundColor: const Color(0xff02b140),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 8,
+                          )),
+                      child: Text(
+                        'Join now',
+                        style: kTextstyle3.copyWith(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(),
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: Chip(
+                      labelPadding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 2),
+                      backgroundColor: Colors.red,
+                      label: Text(
+                        'Live ⚪',
+                        style: kTextstyle3.copyWith(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomeAppbar extends StatelessWidget {
+  const HomeAppbar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          Text(
+            'Home',
+            style: kTextstyle1,
+          ),
+          const Spacer(),
+          IconButton(
+            onPressed: () {
+              log('notification pressed');
+            },
+            constraints: const BoxConstraints(),
+            padding: EdgeInsets.zero,
+            icon: const Icon(
+              Icons.notifications,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(width: 8),
+          IconButton(
+            onPressed: () {
+              log('menu pressed');
+            },
+            constraints: const BoxConstraints(),
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/home/widget/carousel.dart
@@ -0,0 +1,181 @@
+import 'dart:developer';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gathrr/home/bloc/home_bloc.dart';
+import 'package:gathrr/home/model/event.dart';
+import 'package:gathrr/theme/theme.dart';
+
+class Carousel extends StatelessWidget {
+  const Carousel({Key? key}) : super(key: key);
+
+  static final PageController _controller =
+      PageController(initialPage: 1, viewportFraction: 0.9);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final upcomingEvents = context.select((HomeBloc bloc) => bloc.state.events
+        .where((e) => e.getEventType() == EventType.upcoming)
+        .toList());
+    final isLoading = context.select((HomeBloc bloc) => bloc.state.isLoading);
+    final hasError = context.select((HomeBloc bloc) => bloc.state.hasError);
+
+    return SizedBox(
+      width: size.width,
+      height: size.height / 4,
+      child: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: PageView.builder(
+              controller: _controller,
+              onPageChanged: (val) {
+                context.read<HomeBloc>().add(CarouselSwipe(val));
+              },
+              itemBuilder: (context, index) {
+                if (isLoading) {
+                  return const CarouselItem.loading();
+                } else {
+                  if (hasError) {
+                    return const CarouselItem.error();
+                  } else {
+                    return CarouselItem(
+                      title: upcomingEvents[index].eventName,
+                      img: upcomingEvents[index].event_banner_img1,
+                      link: upcomingEvents[index].isBooking,
+                    );
+                  }
+                }
+              },
+              itemCount: isLoading || hasError ? 3 : upcomingEvents.length,
+            ),
+          ),
+          const SizedBox(height: 12),
+        ],
+      ),
+    );
+  }
+}
+
+class CarouselItem extends StatelessWidget {
+  const CarouselItem(
+      {Key? key,
+      this.title,
+      this.img,
+      this.link,
+      this.isLoading = false,
+      this.hasError = false})
+      : super(key: key);
+
+  final String? title, img, link;
+  final bool isLoading, hasError;
+
+  const CarouselItem.loading()
+      : title = 'Loading...',
+        img = '',
+        link = '',
+        isLoading = true,
+        hasError = false;
+  const CarouselItem.error()
+      : title = '',
+        img = '',
+        link = '',
+        isLoading = false,
+        hasError = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      color: Colors.grey,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Container(
+          decoration: hasError || isLoading
+              ? const BoxDecoration()
+              : BoxDecoration(
+                  image: DecorationImage(
+                    image: CachedNetworkImageProvider(
+                      img!,
+                    ),
+                    colorFilter: ColorFilter.mode(
+                      Colors.black.withOpacity(0.7),
+                      BlendMode.darken,
+                    ),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+          padding: const EdgeInsets.all(24.0),
+          child: hasError
+              ? const Center(
+                  child: Icon(
+                    Icons.error_outline_rounded,
+                    color: Colors.red,
+                  ),
+                )
+              : Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title!,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: kTextstyle2.copyWith(color: Colors.white),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        log(img!);
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: const Color(0xffffff00),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      child: Text(
+                        'Book now',
+                        style: kTextstyle3,
+                      ),
+                    ),
+                  ],
+                ),
+        ),
+      ),
+    );
+  }
+}
+
+//carousel indicators
+class CarouselIndicators extends StatelessWidget {
+  const CarouselIndicators({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final isLoading = context.select((HomeBloc bloc) => bloc.state.isLoading);
+    final hasError = context.select((HomeBloc bloc) => bloc.state.hasError);
+    final events = context.select((HomeBloc bloc) =>
+        bloc.state.events.where((e) => e.getEventType() == EventType.upcoming));
+    final index = context.select((HomeBloc bloc) => bloc.state.carouselIndex);
+    return Row(
+      children: [
+        for (var i = 0; isLoading || hasError ? i < 3 : i < events.length; i++)
+          Container(
+            width: i == index ? 10 : 8,
+            height: i == index ? 10 : 8,
+            margin: const EdgeInsets.only(right: 4),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: i == index ? Colors.black : const Color(0xffd2d2d2),
+            ),
+          ),
+      ],
+    );
+  }
+}
